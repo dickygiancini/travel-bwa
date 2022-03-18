8fd56b2b5cf88b71f3aad78600ad446ba5aeaff6
dickygiancini <dicky.giancini.123@gmail.com>
2022-03-18 15:43:22 +0700

    Navigation to buttons page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.admin.dashboard');
 });
+
+Route::get('/buttons', function() {
+    return view('pages.admin.buttons');
+})->name('buttons');
